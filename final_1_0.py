dcb0c12090065cb5866ef2f4bc62ca3880fe2cc2
Mikhail <mikerar329@gmail.com>
2020-02-03 00:30:17 +0300

    Corrections

--- final_1_0.py
+++ final_1_0.py
@@ -11,25 +11,26 @@ from sklearn.metrics import mean_squared_error
 numpy.random.seed(7)
 
 dataset = pandas.read_csv('DUQ_hourly.csv', usecols=[1])
-print(type(dataset))
 plt.plot(dataset)
 plt.show()
 
-scaler = MinMaxScaler(feature_range=(0,1))
+scaler = MinMaxScaler(feature_range=(0, 1))
 dataset = scaler.fit_transform(dataset)
 
-start_index = int(len(dataset)*0.9)
-train_size = int(len(dataset)*0.91)
-train, test = dataset[start_index:train_size,:], dataset[train_size:len(dataset),:]
+start_index = int(len(dataset) * 0.9)
+train_size = int(len(dataset) * 0.9995)
+train, test = dataset[start_index:train_size, :], dataset[train_size:len(dataset), :]
 print(len(train), len(test))
 
+# preparing dataset for LSTM net
 def create_dataset(dataset, look_back=1):
-	dataX, dataY = [], []
-	for i in range(len(dataset)-look_back-1):
-		a = dataset[i:(i+look_back), 0]
-		dataX.append(a)
-		dataY.append(dataset[i + look_back, 0])
-	return numpy.array(dataX), numpy.array(dataY)
+    dataX, dataY = [], []
+    for i in range(len(dataset) - look_back - 1):
+        a = dataset[i:(i + look_back), 0]
+        dataX.append(a)
+        dataY.append(dataset[i + look_back, 0])
+    return numpy.array(dataX), numpy.array(dataY)
+
 
 look_back = 1
 
@@ -39,7 +40,6 @@ testX, testY = create_dataset(test, look_back)
 trainX = numpy.reshape(trainX, (trainX.shape[0], 1, trainX.shape[1]))
 testX = numpy.reshape(testX, (testX.shape[0], 1, testX.shape[1]))
 
-# create and fit the LSTM network
 
 model = Sequential()
 model.add(LSTM(4, input_shape=(1, look_back)))
@@ -47,42 +47,43 @@ model.add(Dense(1))
 model.compile(loss='mean_squared_error', optimizer='adam')
 model.fit(trainX, trainY, epochs=3, batch_size=1, verbose=1)
 
-# make predictions
 
+#pridictions
 trainPredict = model.predict(trainX)
 testPredict = model.predict(testX)
 
-# invert predictions
 
 trainPredict = scaler.inverse_transform(trainPredict)
 trainY = scaler.inverse_transform([trainY])
 testPredict = scaler.inverse_transform(testPredict)
 testY = scaler.inverse_transform([testY])
 
-# calculate root mean squared error
+# calculate errors
 
-trainScore = math.sqrt(mean_squared_error(trainY[0], trainPredict[:,0]))
+trainScore = math.sqrt(mean_squared_error(trainY[0], trainPredict[:, 0]))
 print('Train Score: %.2f RMSE' % (trainScore))
 
-testScore = math.sqrt(mean_squared_error(testY[0], testPredict[:,0]))
+testScore = math.sqrt(mean_squared_error(testY[0], testPredict[:, 0]))
 print('Test Score: %.2f RMSE' % (testScore))
 
-# shift train predictions for plotting
+
+
+# train predictions and plotting
 
 trainPredictPlot = numpy.empty_like(dataset)
 trainPredictPlot[:, :] = numpy.nan
-trainPredictPlot[look_back:len(trainPredict)+look_back, :] = trainPredict
+trainPredictPlot[look_back:len(trainPredict) + look_back, :] = trainPredict
 
-# shift test predictions for plotting
+# test predictions for plotting
 
 testPredictPlot = numpy.empty_like(dataset)
 testPredictPlot[:, :] = numpy.nan
-testPredictPlot[len(trainPredict)+(look_back*2)+1:len(dataset)-look_back-start_index, :] = testPredict
+testPredictPlot[len(trainPredict) + (look_back * 2) + 1:len(dataset) - look_back - start_index, :] = testPredict
 
-# plot baseline and predictions
+# plot
 
 plt.plot(scaler.inverse_transform(dataset[start_index:]))
 plt.plot(trainPredictPlot)
 plt.plot(testPredictPlot)
-plt.legend(['Original','Train', 'Test'], loc='upper right')
+plt.legend(['Original', 'Train', 'Test'], loc='upper right')
 plt.show()
