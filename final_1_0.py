4dd07a070c375b64611baaf1f376559a81a1748d
Mikhail <mikerar329@gmail.com>
2020-02-03 21:02:37 +0300

    Added supports for different datasets

--- final_1_0.py
+++ final_1_0.py
@@ -9,8 +9,8 @@ from sklearn.preprocessing import MinMaxScaler
 from sklearn.metrics import mean_squared_error
 
 numpy.random.seed(7)
-
-dataset = pandas.read_csv('DUQ_hourly.csv', usecols=[1])
+# switch usecols for different datasets
+dataset = pandas.read_csv('PJME_hourly.csv', usecols=[1])
 plt.plot(dataset)
 plt.show()
 
@@ -18,9 +18,10 @@ scaler = MinMaxScaler(feature_range=(0, 1))
 dataset = scaler.fit_transform(dataset)
 
 start_index = int(len(dataset) * 0.9)
-train_size = int(len(dataset) * 0.9995)
+train_size = int(len(dataset) * 0.95)
 train, test = dataset[start_index:train_size, :], dataset[train_size:len(dataset), :]
 print(len(train), len(test))
+
 
 # preparing dataset for LSTM net
 def create_dataset(dataset, look_back=1):
@@ -40,18 +41,15 @@ testX, testY = create_dataset(test, look_back)
 trainX = numpy.reshape(trainX, (trainX.shape[0], 1, trainX.shape[1]))
 testX = numpy.reshape(testX, (testX.shape[0], 1, testX.shape[1]))
 
-
 model = Sequential()
 model.add(LSTM(4, input_shape=(1, look_back)))
 model.add(Dense(1))
 model.compile(loss='mean_squared_error', optimizer='adam')
 model.fit(trainX, trainY, epochs=3, batch_size=1, verbose=1)
 
-
-#pridictions
+# predictions
 trainPredict = model.predict(trainX)
 testPredict = model.predict(testX)
-
 
 trainPredict = scaler.inverse_transform(trainPredict)
 trainY = scaler.inverse_transform([trainY])
@@ -65,8 +63,6 @@ print('Train Score: %.2f RMSE' % (trainScore))
 
 testScore = math.sqrt(mean_squared_error(testY[0], testPredict[:, 0]))
 print('Test Score: %.2f RMSE' % (testScore))
-
-
 
 # train predictions and plotting
 
@@ -83,7 +79,7 @@ testPredictPlot[len(trainPredict) + (look_back * 2) + 1:len(dataset) - look_back
 # plot
 
 plt.plot(scaler.inverse_transform(dataset[start_index:]))
-plt.plot(trainPredictPlot)
-plt.plot(testPredictPlot)
+plt.plot(trainPredictPlot[1:])
+plt.plot(testPredictPlot[1:])
 plt.legend(['Original', 'Train', 'Test'], loc='upper right')
 plt.show()
